d732f9ac32299ddc6cc8fc9f232df128e8c0d1e4
sb3h <470441372@qq.com>
2017-05-22 13:34:26 +0800

    03-离线计算我的商城中热门商品

---
+++ 02realTimeComputeAddressOrderAmountByScala/src/test/scala/com/hhh/temp/HelloScalaJunit.scala
@@ -0,0 +1,16 @@
+package com.hhh.temp
+
+import org.junit.Test
+
+/**
+  * Created by huanghh on 2017/5/16.
+  */
+class HelloScalaJunit {
+
+
+  @Test def testScala() {
+    System.out.println("com.hhh.temp.HelloScalaJunit")
+  }
+
+
+}
